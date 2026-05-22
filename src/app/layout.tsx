d4f7247f261675871875ed19bb038bce9be3061e
Valentino Nathan <nathan070907@gmail.com>
2026-05-22 18:52:43 +0700

    minor fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ const roboto = Roboto({
 export const metadata: Metadata = {
   title: "NUSphere",
   description: "One Campus, One Sphere",
+  icons: {
+    icon: "../../public/NUSPHERE Logo.png",
+  },
 };
 
 export default function RootLayout({
@@ -48,7 +51,7 @@ export default function RootLayout({
             <div >
               <Search />
             </div>
-            <div className="flex h-11 gap-3 items-center">
+            <div className="flex h-11 gap-4.5 items-center">
               <Button className="rounded-md max-h-8.5 border-none font-roboto bg-black/30 hover:bg-black/40 backdrop-blur-3xl hover:cursor-pointer">Log In</Button>
               <Button className="rounded-md max-h-8.5 border-none font-roboto bg-black/20 hover:bg-black/30 hover:cursor-pointer">Sign Up</Button>
               <AvatarWithOnline />
@@ -56,43 +59,43 @@ export default function RootLayout({
           </header>
 
           <main className="flex h-full">
-            <aside className="pt-6.5 px-7.5 sticky min-h-full shadow-md bg-gradient-to-b from-primary/40 from-0% via-secondary/40 via-110% to-secondary/40 to-100% backdrop-blur-md" style={{boxShadow:"18px 0 24px rgba(0,0,0,0.1)"}}>
+            <aside className="pt-6.5 px-6.5 sticky min-h-full shadow-md bg-gradient-to-b from-primary/40 from-0% via-secondary/40 via-110% to-secondary/40 to-100% backdrop-blur-md" style={{boxShadow:"18px 0 24px rgba(0,0,0,0.1)"}}>
               <nav className="flex flex-col gap-11 items-start">
-                <div className="flex items-center gap-5 hover:bg-white/30">
+                <div className="flex items-center gap-5 hover:cursor-pointer text-white hover:text-white/60">
                   <div className="min-w-8 min-h-8 flex justify-center items-center">
-                    <GoHomeFill className="text-white/90 text-3xl"/>
+                    <GoHomeFill className=" text-3xl"/>
                   </div>
-                  <h1 className="text-white">Home</h1>
+                  <h1 className="">Home</h1>
                 </div>
-                <div className="flex items-center gap-5 justify-around">
+                <div className="flex items-center gap-5 justify-around hover:cursor-pointer text-white hover:text-white/60">
                   <div className="min-w-8 min-h-8 flex justify-center items-center">
-                    <IoChatbubble className="text-white/90 text-[163%]" />
+                    <IoChatbubble className="text-[163%]" />
                   </div>
-                  <h1 className="text-white">Chat</h1>
+                  <h1 className="">Chat</h1>
                 </div>
-                <div className="flex items-center gap-5 justify-around">
+                <div className="flex items-center gap-5 justify-around hover:cursor-pointer text-white hover:text-white/60">
                   <div className="min-w-8 min-h-8 flex justify-center items-center">
-                    <FaUserGroup className="text-white/90 text-[170%]" />
+                    <FaUserGroup className="text-[170%]" />
                   </div>
-                  <h1 className="text-white">Find People</h1>
+                  <h1 className="">People</h1>
                 </div>
-                <div className="flex items-center gap-5 justify-around">
+                <div className="flex items-center gap-5 justify-around hover:cursor-pointer text-white hover:text-white/60">
                   <div className="min-w-8 min-h-8 flex justify-center items-center">
-                    <BsCalendar3EventFill className="text-white/90 text-[158%]" />
+                    <BsCalendar3EventFill className="text-[158%]" />
                   </div>
-                  <h1 className="text-white">Events</h1>
+                  <h1 className="">Events</h1>
                 </div>
-                <div className="flex items-center gap-5 justify-around">
+                <div className="flex items-center gap-5 justify-around hover:cursor-pointer text-white hover:text-white/60">
                   <div className="min-w-8 min-h-8 flex justify-center items-center">
-                    <FaStore className="text-white/90 text-[168%]" />
+                    <FaStore className="text-[168%]" />
                   </div>
-                  <h1 className="text-white">Marketplace</h1>
+                  <h1 className="">Market</h1>
                 </div>
-                <div className="flex items-center gap-5 justify-around">
+                <div className="flex items-center gap-5 justify-around hover:cursor-pointer text-white hover:text-white/60">
                   <div className="min-w-8 min-h-8 flex justify-center items-center">
-                    <FaBook className="text-white/90 text-[163%]" />
+                    <FaBook className="text-[163%]" />
                   </div>
-                  <h1 className="text-white">Modules</h1>
+                  <h1 className="">Modules</h1>
                 </div>
               </nav>
             </aside>
