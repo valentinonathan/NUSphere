e8e20b3cd71569b62b39152ebd77a3bf5a7966fc
Valentino Nathan <nathan070907@gmail.com>
2026-06-01 15:35:25 +0700

    added home page layout

--- src/app/(main)/(home)/layout.tsx
+++ src/app/(main)/(home)/layout.tsx
@@ -9,6 +9,8 @@ export default function HomeLayout({
   return (
     <div className="flex min-h-full min-w-full w-full">
       {children}
+      <div className="z-1 fixed top-21 right-4" style={{minWidth:"calc((100vw - 12.5rem)/2)", width:"calc((100vw - 12.5rem)/2)", maxWidth:"calc((100vw - 12.5rem)/2)", minHeight:"calc(100vh - 6.25rem)", maxHeight:"calc(100vh - 6.25rem)", height: "calc(100vh - 6.25rem)"}}>
+      </div>
     </div>
   );
 }
