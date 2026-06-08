fe04363c2767a86c240a334e966f96894c8c1542
Valentino Nathan <nathan070907@gmail.com>
2026-06-08 23:34:39 +0700

    Connection to localhost via .env

--- src/app/(main)/(home)/layout.tsx
+++ src/app/(main)/(home)/layout.tsx
@@ -1,15 +1,19 @@
 import "../../globals.css";
 
 
-export default function HomeLayout({
+export default async function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  let response = await fetch(process.env.BACKEND_URL);
+  response = await response.json();
+
   return (
     <div className="flex min-h-full min-w-full w-full">
       {children}
       <div className="z-1 fixed top-21 right-4" style={{minWidth:"calc((100vw - 12.5rem)/2)", width:"calc((100vw - 12.5rem)/2)", maxWidth:"calc((100vw - 12.5rem)/2)", minHeight:"calc(100vh - 6.25rem)", maxHeight:"calc(100vh - 6.25rem)", height: "calc(100vh - 6.25rem)"}}>
+        {response?.[0]?.name}
       </div>
     </div>
   );
