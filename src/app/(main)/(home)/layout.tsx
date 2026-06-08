0062f8edac8c352b30b873aa36d8adaf77acebe6
Valentino Nathan <nathan070907@gmail.com>
2026-06-08 23:57:49 +0700

    deployment fix again

--- src/app/(main)/(home)/layout.tsx
+++ src/app/(main)/(home)/layout.tsx
@@ -6,14 +6,14 @@ export default async function HomeLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  let response = await fetch(process.env.BACKEND_URL);
-  response = await response.json();
+  const response = await fetch(process.env.BACKEND_URL!);
+  const data = await response.json();
 
   return (
     <div className="flex min-h-full min-w-full w-full">
       {children}
       <div className="z-1 fixed top-21 right-4" style={{minWidth:"calc((100vw - 12.5rem)/2)", width:"calc((100vw - 12.5rem)/2)", maxWidth:"calc((100vw - 12.5rem)/2)", minHeight:"calc(100vh - 6.25rem)", maxHeight:"calc(100vh - 6.25rem)", height: "calc(100vh - 6.25rem)"}}>
-        {response?.[0]?.name}
+        {data?.[0]?.name}
       </div>
     </div>
   );
