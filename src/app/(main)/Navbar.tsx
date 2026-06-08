464511c6e162a8ee30a21fc8d81e5c547096e1c6
Valentino Nathan <nathan070907@gmail.com>
2026-06-09 00:00:52 +0700

    deployment fix 4

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
                     <Button className="rounded-md max-h-8.5 border-none font-roboto bg-black/30 hover:bg-black/40 backdrop-blur-3xl hover:cursor-pointer">Log In</Button>
                 </a>
                 <a href="/signup">
-                <Button href="/signup" className="rounded-md max-h-8.5 border-none font-roboto bg-black/20 hover:bg-black/30 hover:cursor-pointer">Sign Up</Button>
+                <Button className="rounded-md max-h-8.5 border-none font-roboto bg-black/20 hover:bg-black/30 hover:cursor-pointer">Sign Up</Button>
                 </a>
                 <a href="/_valentino_nathan">
                     <AvatarWithOnline size="2.5"/>
