9963c8481175abd9219ff6dc8d9b24676aee9c3e
Valentino Nathan <nathan070907@gmail.com>
2026-06-10 15:52:42 +0700

    fixed login logic

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,16 +8,31 @@ import {
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import background from "../../../../public/login-background.png"
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
     const usernameRef = useRef<HTMLInputElement | null>(null);
     const passwordRef = useRef<HTMLInputElement | null>(null);
+    const router = useRouter();
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [usernameWarning, setUsernameWarning] = useState<string>();
+    const [passwordWarning, setPasswordWarning] = useState<string>();
 
     async function handleSubmit(e: React.MouseEvent) {
         e.preventDefault();
         const username = usernameRef?.current?.value;
         const password = passwordRef?.current?.value;
+        if (username?.length == 0) {
+            return setUsernameWarning("Username field should not be empty!");
+        }
+        if (username?.length > 30) {
+            return setUsernameWarning("Username must be lest than 30 words!");
+        }
+        if (password?.length == 0) {
+            return setPasswordWarning("Password field should not be empty!")
+        }
+
         const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/auth/login", {
             method: "POST",
             credentials: "include",
@@ -30,7 +45,11 @@ export default function Login() {
             })
         });
         const data = await response.json();
-        console.log(data?.message);
+        if (response.ok) {
+            router.push("/");
+        } else {
+            setErrorMessage(data?.message);
+        }
     }
 
     return (
@@ -42,13 +61,16 @@ export default function Login() {
             <div className="flex flex-col gap-2">
                 <label>Username</label>
                 <input type="text" ref={usernameRef} name="username" className="bg-white/20 p-1.5 w-80 rounded-sm border-1 border-white/20 shadow-md shadow-black/5" placeholder="Type your username..."></input>
+                {usernameWarning}
             </div>
             <div className="flex flex-col gap-2">
                 <label>Password</label>
                 <input type="password" ref={passwordRef} name="password" className="bg-white/20 p-1.5 w-80 rounded-sm border-1 border-white/20 shadow-md shadow-black/5" placeholder="Type your password..."></input>
+                {passwordWarning}
             </div>
             <p className="text-center">Do not have an account yet? <Link href="/signup" className="underline hover:cursor-pointer">Sign Up</Link></p>
             <button type="submit" onClick={e => handleSubmit(e)} className="min-w-full bg-secondary/70 p-2 rounded-sm border-1 border-white/20 shadow-md shadow-black/5 hover:cursor-pointer hover:bg-secondary/80 transition duration-100 hover:shadow-lg">Sign In</button>
+            {errorMessage}
         </form>
         </div>
         </>
