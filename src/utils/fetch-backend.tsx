effb85e44eae592feef5633ea3e5ccb14989b3a8
Valentino Nathan <nathan070907@gmail.com>
2026-06-13 01:49:49 +0700

    deployment fix

--- src/utils/fetch-backend.tsx
+++ src/utils/fetch-backend.tsx
@@ -1,64 +1,76 @@
 import { cookies } from "next/headers";
 
 export async function fetchBackendServer<T>(endpoint: string, method: string, body?: object): Promise<T>{ // Fetch backend for server component
-    const cookieStore = await cookies();
-    if (method == "GET" || method == "DELETE") {
-        const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + endpoint, {
-            method: method,
-            headers: {
-                "Content-Type": "application/json",
-                Cookie: cookieStore.toString()
-            },
-        });
-        if (!response.ok) {
-            throw new Error(`HTTP ${response.status}`);
+    try {
+        const cookieStore = await cookies();
+        if (method == "GET" || method == "DELETE") {
+            const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + endpoint, {
+                method: method,
+                headers: {
+                    "Content-Type": "application/json",
+                    Cookie: cookieStore.toString()
+                },
+            });
+            if (!response.ok) {
+                console.log("Failed to fetch")
+            }
+            const data = await response.json();
+            return data;
+        } else {
+            const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + endpoint, {
+                method: method,
+                headers: {
+                    "Content-Type": "application/json",
+                    Cookie: cookieStore.toString()
+                },
+                body: JSON.stringify(body)
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP ${response.status}`);
+            }
+            const data = await response.json();
+            return data;
         }
-        const data = await response.json();
-        return data;
-    } else {
-        const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + endpoint, {
-            method: method,
-            headers: {
-                "Content-Type": "application/json",
-                Cookie: cookieStore.toString()
-            },
-            body: JSON.stringify(body)
-        });
-        if (!response.ok) {
-            throw new Error(`HTTP ${response.status}`);
-        }
-        const data = await response.json();
-        return data;
-    }
+    } catch(error: unknown) {
+        const err = error as Error;
+        console.log(err?.message);
+        return null as T;
+    }   
 }
 
 export async function fetchBackendClient<T>(endpoint: string, method: string, body?: object): Promise<T>{ // Fetch backend for client component
-    if (method == "GET" || method == "DELETE") {
-        const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + endpoint, {
-            method: method,
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        if (!response.ok) {
-            throw new Error(`HTTP ${response.status}`);
+    try {
+        if (method == "GET" || method == "DELETE") {
+            const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + endpoint, {
+                method: method,
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP ${response.status}`);
+            }
+            const data = await response.json();
+            return data;
+        } else {
+            const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + endpoint, {
+                method: method,
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body)
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP ${response.status}`);
+            }
+            const data = await response.json();
+            return data;
         }
-        const data = await response.json();
-        return data;
-    } else {
-        const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + endpoint, {
-            method: method,
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body)
-        });
-        if (!response.ok) {
-            throw new Error(`HTTP ${response.status}`);
-        }
-        const data = await response.json();
-        return data;
+    } catch (error: unknown) {
+        const err = error as Error;
+        console.log(err.message);
+        return null as T;
     }
 }
